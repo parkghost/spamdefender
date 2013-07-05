ecf531cf1fc72698033ee5d73a0300410bb1c1c2
root <mail.labs.brandonc.me>
2013-07-05 10:38:30 +0800

    use os.rename instead of read-write-delete file

--- common/file.go
+++ common/file.go
@@ -27,22 +27,5 @@ func CopyFile(src, dst string) error {
 }
 
 func MoveFile(src, dst string) error {
-	srcFile, err := os.Open(src)
-	if err != nil {
-		return err
-	}
-	defer srcFile.Close()
-
-	dstFile, err := os.OpenFile(dst, os.O_CREATE|os.O_TRUNC|os.O_WRONLY, 0644)
-	if err != nil {
-		return err
-	}
-	defer dstFile.Close()
-
-	_, err = io.Copy(dstFile, srcFile)
-	if err != nil {
-		return err
-	}
-
-	return os.Remove(src)
+	return os.Rename(src, dst)
 }
