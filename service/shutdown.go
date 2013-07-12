d1fe592cbfc2173276050587bf12bf576037d1fc
Brandon Chen <parkghost@gmail.com>
2013-07-12 17:03:29 +0800

    add shutdownhook

---
+++ service/shutdown.go
@@ -0,0 +1,24 @@
+package service
+
+import (
+	"sync"
+)
+
+var (
+	shutdownFnList []func()
+	shutdownMux    sync.Mutex
+)
+
+func AddShutdownHook(fun func()) {
+	shutdownMux.Lock()
+	shutdownFnList = append(shutdownFnList, fun)
+	shutdownMux.Unlock()
+}
+
+func Shutdown() {
+	shutdownMux.Lock()
+	for _, fn := range shutdownFnList {
+		fn()
+	}
+	shutdownMux.Unlock()
+}
